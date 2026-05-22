f764496812b69542c82fa02d97bb77c22b3aa683
SimmyKwon <simon.mw.kwon@gmail.com>
2026-05-22 08:31:55 +0100

    Edited __init__.py

--- Website_detector/__init__.py
+++ Website_detector/__init__.py
@@ -1,4 +1,4 @@
-from .Data_Extractor import first_filter, in_depth_filter
+from .Data_Extractor import first_filter, in_depth_filter, age_finder
 from .UI_Window import app
 from .Whitelist_Manager import is_whitelisted
 
